f378fb239a92e8f667aae56ef268b3ad32bd3764
Vamik28 <vamikshah9292@gmail.com>
2024-01-10 11:46:19 +0530

    add Constructor code

--- Program.cs
+++ Program.cs
@@ -1,11 +1,60 @@
 ﻿using System;
 namespace Vamik
 {
+    class Human
+    {
+        //data types by default private;
+        string name;
+        string gender;
+        int age;
+
+        //paremeterize Constructor
+        public Human(string name, string gender, int age)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.age = age;
+        }
+        //zero Argument Constructor
+        public Human()
+        {
+           
+        }
+        public void printInfo()
+        {
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("Age : " + age);
+            Console.WriteLine("gender : " + gender);
+        }
+        public string Name   // property
+        {
+            get { return name; }   // get method
+            set { name = value; }  // set method
+        }
+         public int Age   // property
+        {
+            get { return age; }   // get method
+            set { age = value; }  // set method
+        }
+         public string Gender   // property
+        {
+            get { return gender; }   // get method
+            set { gender = value; }  // set method
+        }
+    }
     class program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Vamik Shah");
+           Human h1 = new Human();
+           h1.Name = "Vamik";
+           h1.Age = 21;
+           h1.Gender = "Male";
+           h1.printInfo();
+
+
         }
     }
 }
